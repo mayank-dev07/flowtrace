b5e8b9a1686a2b808c0bf4ec58c00ec5ddfd9009
mayank-dev07 <devmayank.hts@gmail.com>
2024-06-07 01:06:51 +0530

    landing page

---
+++ src/lib/constant.ts
@@ -0,0 +1,69 @@
+export const clients = [...new Array(10)].map((client, index) => ({
+  href: `/${index + 1}.png`,
+}));
+
+export const products = [
+  {
+    title: "Moonbeam",
+    thumbnail: "/p1.png",
+  },
+  {
+    title: "Cursor",
+    thumbnail: "/p2.png",
+  },
+  {
+    title: "Rogue",
+    thumbnail: "/p3.png",
+  },
+
+  {
+    title: "Editorially",
+    thumbnail: "/p4.png",
+  },
+  {
+    title: "Editrix AI",
+    thumbnail: "/p5.png",
+  },
+  {
+    title: "Pixel Perfect",
+    thumbnail: "/p6.png",
+  },
+
+  {
+    title: "Algochurn",
+    thumbnail: "/p1.png",
+  },
+  {
+    title: "Aceternity UI",
+    thumbnail: "/p2.png",
+  },
+  {
+    title: "Tailwind Master Kit",
+    thumbnail: "/p3.png",
+  },
+  {
+    title: "SmartBridge",
+    thumbnail: "/p4.png",
+  },
+  {
+    title: "Renderwork Studio",
+    thumbnail: "/p5.png",
+  },
+
+  {
+    title: "Creme Digital",
+    thumbnail: "/p6.png",
+  },
+  {
+    title: "Golden Bells Academy",
+    thumbnail: "/p1.png",
+  },
+  {
+    title: "Invoker Labs",
+    thumbnail: "/p2.png",
+  },
+  {
+    title: "E Free Invoice",
+    thumbnail: "/p3.png",
+  },
+];
